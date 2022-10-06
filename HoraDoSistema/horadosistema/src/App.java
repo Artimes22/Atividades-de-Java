bb331ed4491676c7892f7cced1078f5bc83e394b
Artimes22 <artimes.flima@gmail.com>
2022-10-06 18:35:43 -0300

    vendo a hora

---
+++ HoraDoSistema/horadosistema/src/App.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class App {
+        public static void main(String[] args) throws Exception {
+
+            Date data = new Date();
+            System.out.println("A hora do sistema é :");
+            System.out.println(data.toString());
+
+        }
+    }
